3b411db12ce18046c4f8140e9a0a76a93a1e8d63
Julia Norman <endlessxaura@gmail.com>
2022-03-24 01:09:57 -0400

    Heroku Fixes Take 3

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// server.js
 const express = require('express');
+const path = require('path');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 12080);
+app.use(express.static(__dirname + '/dist/cocktails-ui'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/cocktails-ui/index.html'));
+});
+app.listen(process.env.PORT || 8080);
