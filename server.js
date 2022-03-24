800b52555e223dc2af03f3a0b108b7349cf2df7b
Julia Norman <endlessxaura@gmail.com>
2022-03-24 01:04:38 -0400

    Heroku Fix?

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+// server.js
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +
-        '/dist/<app-name>/index.html'));
-});
-app.listen(process.env.PORT || 8080);
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 12080);
